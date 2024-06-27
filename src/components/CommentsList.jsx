74f2ad054a7e97458181a536276c3fd4accfc2fd
C043 <mariofragnitoph@gmail.com>
2024-06-27 22:41:25 +0200

    Removed useless code

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -4,22 +4,21 @@ import SingleComment from "./SingleComment";
 class CommentsList extends Component {
   render() {
     console.log("------>", this.props.reviews);
-    {
-      return (
-        <div className="mt-2">
-          {this.props.reviews.length > 0 &&
-            this.props.reviews.map(review => (
-              <SingleComment
-                key={review._id}
-                id={review._id}
-                comment={review.comment}
-                author={review.author}
-                rate={review.rate}
-              />
-            ))}
-        </div>
-      );
-    }
+
+    return (
+      <div className="mt-2">
+        {this.props.reviews.length > 0 &&
+          this.props.reviews.map(review => (
+            <SingleComment
+              key={review._id}
+              id={review._id}
+              comment={review.comment}
+              author={review.author}
+              rate={review.rate}
+            />
+          ))}
+      </div>
+    );
   }
 }
 
