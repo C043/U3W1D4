5fb3236d79e97d33697a93948200f56c40af4040
Mario Fragnito <mariofragnitoph@gmail.com>
2024-06-27 17:48:45 +0200

    WIP delete option

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -9,7 +9,13 @@ class CommentsList extends Component {
         <div className="mt-2">
           {this.props.reviews.length > 0 &&
             this.props.reviews.map(review => (
-              <SingleComment key={review._id} comment={review.comment} author={review.author} rate={review.rate} />
+              <SingleComment
+                key={review._id}
+                id={review._id}
+                comment={review.comment}
+                author={review.author}
+                rate={review.rate}
+              />
             ))}
         </div>
       );
--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,11 +1,37 @@
-const SingleComment = props => {
-  return (
-    <div className="d-flex flex-column justify-content-center align-items-center text-center">
-      <p>
-        {props.author} gave {props.rate}/5 <br /> "{props.comment}"
-      </p>
-    </div>
-  );
-};
+import { Component } from "react";
+
+class SingleComment extends Component {
+  handleDelete = async () => {
+    try {
+      const resp = await fetch("https://striveschool-api.herokuapp.com/api/comments/", {
+        headers: {
+          Authorization:
+            "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZWQyNjdjMjM5YzAwMTUyZjRiMmUiLCJpYXQiOjE3MTk0ODc0NjQsImV4cCI6MTcyMDY5NzA2NH0.etOLICwJO7zEB3M0sNrl4SLSRePOVrlhw7mIBhrmOfE",
+          "Content-Type": "application/json",
+        },
+        method: "DELETE",
+        body: JSON.stringify(this.props.id),
+      });
+      if (resp.ok) {
+        window.alert("Recensione eliminata!");
+      } else {
+        throw new Error("Errore nella cancellazione del dato");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    console.log(this.props.id);
+    return (
+      <div className="d-flex flex-column justify-content-center align-items-center text-center">
+        <p>
+          {this.props.author} gave {this.props.rate}/5 <br /> "{this.props.comment}"
+        </p>
+      </div>
+    );
+  }
+}
 
 export default SingleComment;
