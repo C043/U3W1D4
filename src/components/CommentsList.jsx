3c7e0c5f56b9da3b08a8dc25c403c706ce4e3d92
Mario Fragnito <mariofragnitoph@gmail.com>
2024-06-27 16:59:19 +0200

    Fixed comments

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -7,7 +7,9 @@ class CommentsList extends Component {
     {
       return (
         this.props.reviews.length > 0 &&
-        this.props.reviews.map(review => <SingleComment key={review._id} comment={review.comment} />)
+        this.props.reviews.map(review => (
+          <SingleComment key={review._id} comment={review.comment} author={review.author} rate={review.rate} />
+        ))
       );
     }
   }
--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,6 +1,11 @@
-const SingleComment = comment => {
-  console.log(comment);
-  return <p>{comment.comment}</p>;
+const SingleComment = props => {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center text-center">
+      <p>
+        {props.author} gave {props.rate}/5 <br /> "{props.comment}"
+      </p>
+    </div>
+  );
 };
 
 export default SingleComment;
